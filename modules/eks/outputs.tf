8ccc0bb1b3dc6b8b8d6ae8d846ad6959201647e8
kiranjavvadi <74632910+kiranjavvadi@users.noreply.github.com>
2025-06-07 00:42:01 +0530

    Create outputs.tf

---
+++ modules/eks/outputs.tf
@@ -0,0 +1,24 @@
+output "cluster_id" {
+  description = "ID of the EKS cluster"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_security_group.eks_cluster.id
+}
+
+output "cluster_ca_cert" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
