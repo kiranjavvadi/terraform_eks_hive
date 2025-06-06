26ac8b7b5fd569ac4a18fd7b071dc0e08c9f6d9f
kiranjavvadi <74632910+kiranjavvadi@users.noreply.github.com>
2025-06-07 00:38:13 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
