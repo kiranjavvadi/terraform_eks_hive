f769f6c4bdd073002b67449d20e6487ffd9a0a63
kiranjavvadi <74632910+kiranjavvadi@users.noreply.github.com>
2025-06-07 00:41:19 +0530

    Create variables.tf

---
+++ modules/eks/variables.tf
@@ -0,0 +1,44 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "instance_types" {
+  description = "EC2 instance types for the node group"
+  type        = list(string)
+}
+
+variable "min_size" {
+  description = "Minimum number of nodes"
+  type        = number
+}
+
+variable "max_size" {
+  description = "Maximum number of nodes"
+  type        = number
+}
+
+variable "desired_size" {
+  description = "Desired number of nodes"
+  type        = number
+}
